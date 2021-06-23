a31924761c6659834aa488a59086a44b01769f5d
exfirewall <zpqls321@naver.com>
2021-06-23 14:46:53 +0900

    add main

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -14,117 +14,119 @@ import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.iBrowser;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.BClazz;
+import com.company.design.singleton.SocketClient;
 import com.company.design.strategy.Base64Strategy;
 import com.company.design.strategy.Encoder;
 import com.company.design.strategy.EncodingStrategy;
 import com.company.design.strategy.NormalStrategy;
-
-import javax.swing.text.DefaultEditorKit;
-import java.util.WeakHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Aclazz aclazz = new Aclazz();
-//        BClazz bClazz = new BClazz();
-//
-//        SocketClient aClient = aclazz.getSocketClient();
-//        SocketClient bClient = bClazz.getSocketClient();
-//
-//        System.out.println("객체가 동일한가?");
-//        System.out.println(aClient.equals(bClient));
+        // singleton pattern
+        Aclazz aclazz = new Aclazz();
+        BClazz bClazz = new BClazz();
 
-//        HairDryer hairDryer = new HairDryer();
-//        connect(hairDryer);
-//
-//        Cleaner cleaner = new Cleaner();
-//        Electronic110v CleanerAdapter = new SocketAdapter(cleaner);
-//        connect(CleanerAdapter);
-//        AirConditioner airConditioner = new AirConditioner();
-//        Electronic110v AirAdapter = new SocketAdapter(airConditioner);
-//        connect(AirAdapter);
+        SocketClient aClient = aclazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
 
-//        Browser browser = new Browser("www.naver.com");
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//
-//        BrowserProxy browser2 = new BrowserProxy("www.naver.com");
-//        browser2.show();
-//        browser2.show();
-//        browser2.show();
-//        browser2.show();
+        System.out.println("객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
 
-//        AtomicLong start = new AtomicLong();
-//        AtomicLong end = new AtomicLong();
-//
-//        iBrowser aopBrowser = new AopBrowser("www.naver.com",
-//                ()->{
-//                    System.out.println("before");
-//                    start.set(System.currentTimeMillis());
-//                },
-//                ()->{
-//                    long now = System.currentTimeMillis();
-//                    end.set(now - start.get());
-//                }
-//                );
-//
-//        aopBrowser.show();
-//        System.out.println("loading time : " + end.get());
-//
-//        aopBrowser.show();
-//        System.out.println("loading time : " + end.get());
+        //adapter pattern
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
 
-//        Icar audi = new Audi(1000);
-//        audi.showPrice();
-//
-//        // a3
-//        Icar audi3 = new A3(audi, "A3");
-//        audi3.showPrice();
-//        // a4
-//        Icar audi4 = new A4(audi, "A4");
-//        audi4.showPrice();
-//        // a5
-//        Icar audi5 = new A5(audi, "A5");
-//        audi5.showPrice();
+        Cleaner cleaner = new Cleaner();
+        Electronic110v CleanerAdapter = new SocketAdapter(cleaner);
+        connect(CleanerAdapter);
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110v AirAdapter = new SocketAdapter(airConditioner);
+        connect(AirAdapter);
 
-//        Button button = new Button("버튼");
-//
-//        button.addListener(new iButtonListener() {
-//            @Override
-//            public void clickEvent(String event) {
-//                System.out.println(event);
-//            }
-//        });
-//
-//        button.click("메시지 전달 : click 1");
-//        button.click("메시지 전달 : click 2");
-//        button.click("메시지 전달 : click 3");
-//        button.click("메시지 전달 : click 4");
+        //proxy pattern
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
 
-//        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
-//        ftpClient.connect();
-//        ftpClient.moveDirectory();
-//
-//        Writer writer = new Writer("text.tmp");
-//        writer.fileConnect();
-//        writer.write();
-//
-//        Reader reader = new Reader("text.tmp");
-//        reader.fileConnect();
-//        reader.fileRead();
-//
-//        ftpClient.disconnect();
-//        writer.fileDisconnect();
-//        reader.fileDisconnect();
-//
-//        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
-//        sftpClient.connect();
-//        sftpClient.write();
-//        sftpClient.read();
-//        sftpClient.disConnect();
+        BrowserProxy browser2 = new BrowserProxy("www.naver.com");
+        browser2.show();
+        browser2.show();
+        browser2.show();
+        browser2.show();
+
+        //aop
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        iBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+                );
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        //decorator pattern
+        Icar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        Icar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+        // a4
+        Icar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+        // a5
+        Icar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
+
+        //observer pattern
+        Button button = new Button("버튼");
+
+        button.addListener(System.out::println);
+
+        button.click("메시지 전달 : click 1");
+        button.click("메시지 전달 : click 2");
+        button.click("메시지 전달 : click 3");
+        button.click("메시지 전달 : click 4");
+
+        //facade pattern
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        ftpClient.disconnect();
+        writer.fileDisconnect();
+        reader.fileDisconnect();
+
+        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disConnect();
+
+        //strategy pattern
         Encoder encoder = new Encoder();
 
         // base64
